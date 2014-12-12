fcf01350d7a764eb2a513725ff97ace8eb3e10dd
Dhayalan <prakasam.dhayalan@realimage.com>
2014-12-12 08:56:56 +0530

    Added page name in articles

--- client/modules/articles/article-controller.js
+++ client/modules/articles/article-controller.js
@@ -1,7 +1,8 @@
 myApp.controller("ArticleController", ['$scope', '$routeParams', 'Article', 
 	function($scope, $routeParams, Article) {
-
-	$scope.articleType = $routeParams.type;
+	var articleType = $routeParams.type;
+	$scope.pageName = articleType.substring(0,1).toUpperCase() + articleType.substring(1, articleType.length-1);
+	$scope.articleType = articleType;
 	$scope.articleId = $routeParams.id;
 	$scope.article = {};
 
--- client/modules/articles/article-list-controller.js
+++ client/modules/articles/article-list-controller.js
@@ -1,7 +1,9 @@
 myApp.controller("ArticleListController", ['$scope', '$routeParams', 'Article', 
 	function($scope, $routeParams, Article) {
-
-	$scope.articleType = $routeParams.type;
+	var articleType = $routeParams.type;
+	$scope.pageName = articleType.substring(0,1).toUpperCase() + articleType.substring(1, articleType.length-1) + 's';
+	
+	$scope.articleType = articleType;
 	$scope.articleList = [];
 
 	if(Article.types[$scope.articleType]) {
